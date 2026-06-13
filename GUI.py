445fd45b557f6b7f9fa3d7c3fd35004b19f2e698
Raziel <sprav89@gmail.com>
2026-06-13 19:50:25 +0300

    Clean up GUI code: remove Mad Lib reference and unused elements

--- GUI.py
+++ GUI.py
@@ -1,47 +1,36 @@
-# Mad Lib
-# Create a story based on user input
+# Click Counter GUI
+# Simple application using Tkinter
 
 from tkinter import *
 
 class Application(Frame):
-    """ GUI application which counts button clicks. """
+    """GUI application which counts button clicks."""
+    
     def __init__(self, master):
-        """ Initialize the frame. """
+        """Initialize the frame."""
         super(Application, self).__init__(master)
         self.grid()
         self.bttn_clicks = 0    # the number of button clicks
         self.create_widget()
 
     def create_widget(self):
-        """ Create button which displays number of clicks. """
+        """Create button which displays number of clicks."""
         self.bttn = Button(self)
-        self.bttn["text"]= "Total Clicks: 0"
+        self.bttn["text"] = "Total Clicks: 0"
         self.bttn["command"] = self.update_count
         self.bttn.grid()
 
     def update_count(self):
-        """ Increase click count and display new total. """
+        """Increase click count and display new total."""
         self.bttn_clicks += 1
         self.bttn["text"] = "Total Clicks: " + str(self.bttn_clicks)
 
-# main
-root = Tk()
-root.title("Простейший Gui")
-root.geometry("500x400")
-
-# внутри окна создается рамка для размещения других элементов
-app = Frame(root)
-app.grid()
-
-# создание метки внутри рамки
-lbl = Label(app, text = "Вот она я!")
-lbl.grid()
-# create a button in the frame
-bttn1 = Button(app, text = "Вот она я!")
-bttn1.grid()
-
-app = Application(root)
-
-root.mainloop()
-
-# input("\n Нажмите ... ")
+# Main
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Простейший GUI")
+    root.geometry("500x400")
+    
+    app = Application(root)
+    
+    root.mainloop()
